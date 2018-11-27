b9b9e1126815522decb93df2e3c353192afd9d51
Z Chen <zchen6464@bths.edu>
2018-11-27 11:43:08 -0500

    Update InPlaceSorts.java

--- src/com/company/InPlaceSorts.java
+++ src/com/company/InPlaceSorts.java
@@ -3,6 +3,40 @@ package com.company;
 import java.util.Arrays;
 
 public class InPlaceSorts {
+    public class QuickSort
+{
+    public void quickSort(int[] arr, int left, int right)
+    {
+        if(left < right)
+        {
+            int pivot = partition(arr,left,right);
+
+            quickSort(arr,left,pivot - 1);
+            quickSort(arr,pivot + 1, right);
+        }
+    }
+
+    public int partition(int[] arr, int left, int right)
+    {
+        int pivot = arr[right];
+        int i = left - 1;
+        int temp;
+        for(int j = left; j < right; j++)
+        {
+            if(arr[j] <= pivot)
+            {
+                i++;
+                temp = arr[j];
+                arr[i] = arr[j];
+                arr[j] = arr[temp];
+            }
+        }
+        temp = arr[right];
+        arr[right] = arr[i+1];
+        arr[i+1] = temp;
+        return (i+1);
+    }
+}
 
     public static void bubbleSort(String[] arr)
     {
