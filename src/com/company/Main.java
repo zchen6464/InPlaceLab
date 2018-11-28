d74c007202d1bda5dd10755b75e6670e1b361bd6
Z Chen <zchen6464@bths.edu>
2018-11-28 10:47:49 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,9 +9,11 @@ public class Main {
         int[] test1 = {1,5,4,3,2};
         double[] test2 = {1.0,5.0,4.0,3.0,2.0};
         String[] test3 = {"d","b","a","c"};
+        int[] test4 = {1,5,4,3,2};
         InPlaceSorts.insertionSort(test1);
         InPlaceSorts.selctionSort(test2);
         InPlaceSorts.bubbleSort(test3);
-        InPlaceSorts.quickSort(test1,0,4);
+        InPlaceSorts.quickSort(test4,0,test4.length-1);
+        System.out.println(Arrays.toString(test4));
     }
 }
