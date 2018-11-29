f1189a2bd9a9ee345ea8a3c572235027436b844b
Z Chen <zchen6464@bths.edu>
2018-11-29 10:37:45 -0500

    Update InPlaceSorts.java

--- src/com/company/InPlaceSorts.java
+++ src/com/company/InPlaceSorts.java
@@ -1,4 +1,17 @@
-right)
+package com.company;
+ import java.util.Arrays;
+ public class InPlaceSorts {
+    
+    public static void quickSort(int[] arr, int left, int right)
+    {
+        if(left < right)
+        {
+            int pivot = partition(arr,left,right);
+             quickSort(arr,left,pivot - 1);
+            quickSort(arr,pivot + 1, right);
+        }
+    }
+     public static int partition(int[] arr, int left, int right)
     {
         int pivot = arr[right];
         int i = left - 1;
